d1e435d10d0ff2ac2129a36f1bd358eda13aba95
Szymon Dzik <sz.dzik@outlook.com>
2022-12-27 17:52:48 +0100

    Initialise project

---
+++ uefa-probability/src/main/java/sports/uefaprobability/UefaProbabilityApplication.java
@@ -0,0 +1,13 @@
+package sports.uefaprobability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UefaProbabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UefaProbabilityApplication.class, args);
+	}
+
+}
---
+++ uefa-probability/src/test/java/sports/uefaprobability/UefaProbabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package sports.uefaprobability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UefaProbabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
